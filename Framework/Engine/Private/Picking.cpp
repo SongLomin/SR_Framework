ea2ad8eaa782a703e353546a146c40feacae5b8e
gksmssla82 <97272988+gksmssla82@users.noreply.github.com>
2022-06-19 17:39:35 +0900

    Add Picking

--- Framework/Engine/Private/Picking.cpp
+++ Framework/Engine/Private/Picking.cpp
@@ -61,8 +61,46 @@ void CPicking::Compute_RayInWorldSpace()
 
 }
 
+_bool CPicking::Picking(CVIBuffer* pVIBuffer, CTransform* pTransform, _float3* pOut)
+{
+
+	_float4x4		WorldMatrix = pTransform->Get_WorldMatrix();                 // 월드 메트릭스 가져옴
+	D3DXMatrixInverse(&WorldMatrix, nullptr, &WorldMatrix);    // 열행렬 곱해 로컬스페이스로 감
+
+	_float3			vRayPos, vRayDir;                                           //  Ray위치백터, Ray방향백터 
+
+	D3DXVec3TransformCoord(&vRayPos, &m_vRayPos, &WorldMatrix);      // Ray위치백터 -> 로컬스페이스로보냄 
+	D3DXVec3TransformNormal(&vRayDir, &m_vRayDir, &WorldMatrix);     // Ray방향백터 -> 로컬스페이스로보냄
+	D3DXVec3Normalize(&vRayDir, &vRayDir);                              // Ray방향백터 정규화
+
+	_uint iNumFaces = pVIBuffer->Get_NumPrimitive();                           // VIBuffer에있는 폴리곤 수 가져옴
+	const _float3* pVerticesPos = pVIBuffer->Get_VerticesPos();                // VIBuffer에있는 버텍스 위치 가져옴
+
+	_float  fU, fV, fDist;
+
+	for (_uint i = 0; i < iNumFaces; ++i)                                     // 폴리곤 수만큼 반복
+	{
+		_uint3 iIndices = pVIBuffer->Get_Indices(i);                    // 해당 폴리곤 인덱스 가져옴
+
+		_float3		vPickedPos;
+
+		if (true == D3DXIntersectTri(&pVerticesPos[iIndices.ix], &pVerticesPos[iIndices.iy], &pVerticesPos[iIndices.iz], &vRayPos, &vRayDir, &fU, &fV, &fDist))
+		{
+			vPickedPos = vRayPos + *D3DXVec3Normalize(&vRayDir, &vRayDir) * fDist;
+			D3DXVec3TransformCoord(pOut, &vPickedPos, &pTransform->Get_WorldMatrix());
+
+			return true;
+		}
+
+	}
+
+	return false;
+}
 
 
 void CPicking::Free()
 {
+	__super::Free();
+
+	delete this;
 }
