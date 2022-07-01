7e5fe2ab340ba016d636aa906ce68de2b95b9d73
gksmssla82 <97272988+gksmssla82@users.noreply.github.com>
2022-07-01 18:29:35 +0900

    Planet_Level

---
+++ Framework/Client/Public/Level_MagmaPlanet.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Client_Defines.h"
+#include "Level.h"
+
+BEGIN(Client)
+
+class CLevel_SelectPlanet final : public CLevel
+{
+public:
+	CLevel_SelectPlanet();
+	virtual ~CLevel_SelectPlanet() = default;
+
+public:
+	virtual HRESULT Initialize();
+	virtual void Tick(_float fTimeDelta);
+	virtual HRESULT Render();
+
+
+
+public:
+	static CLevel_SelectPlanet* Create();
+	virtual void Free() override;
+};
+
+END
+
