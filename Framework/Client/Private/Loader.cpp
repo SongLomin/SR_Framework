d2402862291ec0ef66885655e31cb411769354d1
gksmssla82 <97272988+gksmssla82@users.noreply.github.com>
2022-07-12 02:13:18 +0900

    Add

--- Framework/Client/Private/Loader.cpp
+++ Framework/Client/Private/Loader.cpp
@@ -171,11 +171,13 @@ HRESULT CLoader::Loading_ForLogoLevel()
 		TEXTURE_TYPE::TYPE_DEFAULT, MEMORY_TYPE::MEMORY_STATIC)))
 		return E_FAIL;
 
-
 	if (FAILED(GAMEINSTANCE->Load_Textures(TEXT("Benner"), TEXT("../Bin/Resources/Textures/UI/Benner.png"),
 		TEXTURE_TYPE::TYPE_DEFAULT, MEMORY_TYPE::MEMORY_STATIC)))
 		return E_FAIL;
 
+	if (FAILED(GAMEINSTANCE->Load_Textures(TEXT("Font_UI"), TEXT("../Bin/Resources/Textures/UI/Font_%d.png"),
+		TEXTURE_TYPE::TYPE_DEFAULT, MEMORY_TYPE::MEMORY_STATIC)))
+		return E_FAIL;
 
 	if (!GAMEINSTANCE->Add_GameObject<CLoading>(LEVEL_LOADING, TEXT("Loading")))
 		return E_FAIL;
